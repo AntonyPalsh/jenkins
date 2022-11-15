0d1d37979538393711b545b35290f57c5be22629
Anton <anton@gmail.com>
2022-11-15 17:22:01 +0100

    #

--- producer/entrypoint.sh
+++ producer/entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
-apk add --update --no-cache py3-pip && pip install kafka-python
-
 python3 producer.py $KAFKA_HOST
